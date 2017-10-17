f74d3bcd40842cd1622da8c4b6b432cc1cade73b
Jonas Dahl <jonas@jdahl.se>
2017-10-17 22:43:24 +0200

    Added picture of winner

--- resources/views/ended.blade.php
+++ resources/views/ended.blade.php
@@ -11,6 +11,7 @@
 </header>
 <div id="content">
 	@foreach ($sm->winningBets() as $wb) 
+	<div class="crop" style="margin: 0 auto;float:none;position:static;display:block;width: 100px; height: 100px; background-image: url('https://zfinger.datasektionen.se/user/{{ $wb->user->kth_username }}/image')"></div>
     <h1 style="text-align: center">{{ $wb->user->name }} vann med bettet {{ $wb->time->format('H:i') }}.</h1>
     @endforeach
     <h2>Alla gissningar</h2>
