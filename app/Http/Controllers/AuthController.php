d77b9a62be157de69c9a385c1c94e6365c27b6b9
laxsjo <rasmus.soderhielm@gmail.com>
2025-11-10 11:41:38 +0100

    feat: Migrate from login to sso
    This required adding jumbojett/openid-connect-php as a dependency, as
    well as fixing the nginx configuration in the Docker Compose dev
    environment.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,17 +7,30 @@ use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 
 use App\User;
-use Auth;
-use Session;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Jumbojett\OpenIDConnectClient;
 
 /**
-* Authentication controller. Handles login via login2.datasektionen.se.
+* Authentication controller. Handles login via sso.datasektionen.se.
 *
-* @author Jonas Dahl <jonas@jdahl.se>
-* @version 2016-11-23
+* @author Jonas Dahl <jonas@jdahl.se>, Rasmus Söderhielm <rasmus.soderhielm@gmail.com>
+* @version 2025-11-10
 */
 class AuthController {
 	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    private OpenIDConnectClient $oidc;
+
+	function __construct() {
+        $this->oidc = new OpenIDConnectClient(
+            env('OIDC_PROVIDER'),
+            env('OIDC_ID'),
+            env('OIDC_SECRET')
+        );
+        $this->oidc->setRedirectURL(env('REDIRECT_URL'));
+    }
 
 	/**
 	* The logout url. Redirects to main page with success message.
@@ -32,40 +45,29 @@ class AuthController {
 	}
 
 	/**
-	* The login page. Just redirects to login2.
+	* The login page. Just redirects to sso.
 	* 
-	* @return redirect to login2.datasektionen.se
+	* @return redirect to sso.datasektionen.se
 	*/
 	public function getLogin(Request $request) {
-		return redirect(env('LOGIN_FRONTEND_URL') . '/login?callback=' . url('/login-complete') . '/');
+		return $this->oidc->authenticate();
 	}
 
 	/**
-	* When login is complete, login2 will redirect us here. Now verify the login.
+	* When login is complete, sso will redirect us here. Now verify the login.
 	* 
-	* @param  string $token the token from login2
 	* @return redirect to main page or intended page
 	*/
-	public function getLoginComplete($token) {
-		// Send get request to login server
-		$client = new Client();
-		$res = $client->request('GET', env('LOGIN_API_URL') . '/verify/' . $token . '.json', [
-			'form_params' => [
-				'format' => 'json',
-				'api_key' => env('LOGIN_API_KEY')
-			]
-		]);
-
-		// We now have a response. If it is good, parse the json and login user
-		if ($res->getStatusCode() == 200) {
-			$body = json_decode($res->getBody());
-			$user = User::createIfNotExistsOrFail($body->user, $body);
-
-			Auth::login($user);
-		} else {
-			Auth::logout();
+	public function getLoginComplete() {
+		if ($this->oidc->authenticate() === FALSE) {
 			return redirect('/')->with('error', 'Du loggades inte in.');
 		}
+
+        $kthId = $this->oidc->getVerifiedClaims('sub');
+
+		$user = User::createIfNotExistsOrFail($kthId);
+
+		Auth::login($user);
 
 		return redirect()->intended('/')->with('success', 'Du loggades in.');
 	}
